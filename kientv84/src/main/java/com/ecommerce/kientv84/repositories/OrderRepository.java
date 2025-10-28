34b4312168e1e62bbd21eca7254b611f18812f71
Truong Chi Kien <kientv@MacBook-Pro-cua-Truong.local>
2025-10-28 14:53:41 +0700

    update init source

--- kientv84/src/main/java/com/ecommerce/kientv84/repositories/OrderRepository.java
+++ kientv84/src/main/java/com/ecommerce/kientv84/repositories/OrderRepository.java
@@ -1,4 +1,4 @@
-package com.ecommerce.kientv84.repository;
+package com.ecommerce.kientv84.repositories;
 
 import com.ecommerce.kientv84.entities.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
